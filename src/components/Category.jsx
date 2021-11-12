2a1a04563d9ccb7a0d17987b3791058ec816f915
Luchimen <luchimen@gmail.com>
2021-11-12 18:51:32 -0500

    Se agrego el css de categorias

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,22 +2,26 @@ import React from "react";
 
 export const Category = () => {
   return (
-    <div>
-      <h2>Categorias</h2>
-      <ul>
-        <li>
+    <section className="category container">
+      <h2 className="category__title">Categorias</h2>
+      <div className="category__list">
+        <a href="#">Antibioticos</a>
+        <a href="#">AntiHistaminicos</a>
+        <a href="#">AntiInflamatorios</a>
+        <a href="#">Analgesicos</a>
+        {/* <li className="category__listitem">
           <a href="#">Antibioticos</a>
         </li>
-        <li>
+        <li className="category__listitem">
           <a href="#">AntiHistaminicos</a>
         </li>
-        <li>
+        <li className="category__listitem">
           <a href="#">AntiInflamatorios</a>
         </li>
-        <li>
+        <li className="category__listitem">
           <a href="#">Analgesicos</a>
-        </li>
-      </ul>
-    </div>
+        </li> */}
+      </div>
+    </section>
   );
 };
